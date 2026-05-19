c28f79ecbf2d2cae5019761ef04b3e59b6e345bd
Thirukumaran07 <168185211+Thirukumaran07@users.noreply.github.com>
2026-05-19 20:15:57 +0530

    Time: 1 ms (99.88%), Space: 43.7 MB (10%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,75 @@
-class Solution {
-    public boolean isValid(String s) {
-        Stack <Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='('||ch=='['||ch=='{')
-            st.push(ch);
-            else{
-                if(!st.isEmpty() && st.peek()=='(' && ch == ')'){
-                    st.pop();
-                }
-                else if(!st.isEmpty() && st.peek()=='[' && ch == ']'){
-                    st.pop();
-                }
-                else if(!st.isEmpty() && st.peek()=='{' && ch == '}'){
-                    st.pop();
-                }
-                else{
-                    st.push(ch);
-                }
-            }
+class Node{
+	char data;
+	Node link;
+	Node(char data){
+		this.data=data;
+	}
+}
+class Solution  {
+    	Node head = null;
+	
+		void push(char value) {
+			Node newnode = new Node(value);
+			if(head==null) {
+				head=newnode;
+			}else {
+				newnode.link=head;
+				head=newnode;
+                }		
         }
-        return st.isEmpty();
+		void pop() {
+			if(head!=null) {
+				char value = head.data;
+				head=head.link;			
+				}
+        }
+		char peek() {
+			if(head!=null) {
+				return head.data;			
+                }
+			return '!';		
+        }
+
+    public boolean isValid(String s) {
+        for(char c : s.toCharArray()) {
+				if((c=='(')||(c=='{')||(c=='['))
+				{
+					push(c);
+				}
+				else if((c==')')) {
+					if(head!=null) {
+					if(peek()=='(') {
+						pop();
+					}else {
+						return false;
+					}
+                }else {
+						return false;
+					}
+				}
+				else if((c=='}')) {
+					if(head!=null) {
+					if(peek()=='{') {
+						pop();
+					}else {
+						return false;
+					}
+                }else {
+						return false;
+					}
+				}
+				else if((c==']')) {
+					if(head!=null) {
+					if(peek()=='[') {
+						pop();
+					}else {
+						return false;
+					}
+                }else {
+						return false;
+					}
+				}
+		}
+			return (head==null)?true:false;
     }
 }
