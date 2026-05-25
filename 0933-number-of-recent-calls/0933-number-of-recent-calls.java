70e24601cd76ba0c8dbd69d7e1e1e5c357d3df33
Thirukumaran07 <168185211+Thirukumaran07@users.noreply.github.com>
2026-05-25 10:14:39 +0530

    Time: 22 ms (55.68%), Space: 59.6 MB (50.37%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -6,7 +6,7 @@ class RecentCounter {
     
     public int ping(int t) {
         q.offer(t);
-        while(q.peek()<t-3000){
+        while(!q.isEmpty() && q.peek()<t-3000){
             q.poll();
         }
         return q.size();
