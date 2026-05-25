502c58c23198fb6eb92e5b02d041af736262e2b8
Thirukumaran07 <168185211+Thirukumaran07@users.noreply.github.com>
2026-05-25 11:33:49 +0530

    Time: 13 ms (22.02%), Space: 46.9 MB (62.02%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,22 +1,30 @@
-class Solution {
-    public String predictPartyVictory(String senate) {
+class Solution 
+{
+    public String predictPartyVictory(String senate) 
+    {
         Queue<Integer> rad = new LinkedList<>();
         Queue<Integer> dir = new LinkedList<>();
-        for(int i=0;i<senate.length();i++){
-            if(senate.charAt(i)=='R'){
+        for(int i=0;i<senate.length();i++)
+        {
+            if(senate.charAt(i)=='R')
+            {
                 rad.offer(i);
             }
-            else{
+            else
+            {
                 dir.offer(i);
             }
         }
-        while(!rad.isEmpty() && !dir.isEmpty()){
+        while(!rad.isEmpty() && !dir.isEmpty())
+        {
             int r = rad.poll();
             int d = dir.poll();
-            if(r<d){
+            if(r<d)
+            {
                 rad.offer(r+senate.length());
             }
-            else{
+            else
+            {
                 dir.offer(d+senate.length());
             }
         }
