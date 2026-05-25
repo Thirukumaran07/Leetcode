5c17ffa9ce530fcb2b9514154b15da7341e18fbd
Thirukumaran07 <168185211+Thirukumaran07@users.noreply.github.com>
2026-05-25 11:28:00 +0530

    Time: 12 ms (48.15%), Space: 46.8 MB (62.02%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> rad = new LinkedList<>();
+        Queue<Integer> dir = new LinkedList<>();
+        for(int i=0;i<senate.length();i++){
+            if(senate.charAt(i)=='R'){
+                rad.offer(i);
+            }
+            else{
+                dir.offer(i);
+            }
+        }
+        while(!rad.isEmpty() && !dir.isEmpty()){
+            int r = rad.poll();
+            int d = dir.poll();
+            if(r<d){
+                rad.offer(r+senate.length());
+            }
+            else{
+                dir.offer(d+senate.length());
+            }
+        }
+        return rad.isEmpty()?"Dire":"Radiant";
+    }
+}
