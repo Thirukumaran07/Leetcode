938616af07d9631ed31223203126c49d92f57bbf
Thirukumaran07 <168185211+Thirukumaran07@users.noreply.github.com>
2026-05-25 14:03:03 +0530

    Time: 11 ms (22.62%), Space: 46 MB (28.88%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,24 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+
         Queue<int[]> q = new LinkedList<>();
+
         for (int i = 0; i < tickets.length; i++) {
             q.offer(new int[]{i, tickets[i]});
         }
+
         int time = 0;
+
         while (!q.isEmpty()) {
+
             int[] person = q.poll();
+
             int index = person[0];
             int remaining = person[1];
+
             remaining--;
             time++;
+
             if (remaining == 0) {
+
                 if (index == k) {
                     return time;
                 }
+
             } else {
+
                 q.offer(new int[]{index, remaining});
             }
         }
+
         return time;
     }
 }
